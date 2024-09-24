7d5d5a7a5ffc441ba1acbfe5926562f14ea84fcc
Meet240903 <meetmangroliya24@gmail.com>
2024-09-24 18:36:27 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,121 @@
+import React from 'react'
+import '../assets/css/footer.css'
+import footerLogo from '../assets/images/footerLogo.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+const Footer = () => {
+    const footerData = [
+        {
+            title: "for artists",
+            contentData: [
+                {
+                    content: "For Artists",
+                },
+                {
+                    content: "How it Works",
+                },
+                {
+                    content: "Artist Toolkit",
+                },
+                {
+                    content: "Referral Program",
+                },
+            ]
+        },
+        {
+            title: "for fans",
+            contentData: [
+                {
+                    content: "For Fans",
+                },
+                {
+                    content: "Records",
+                },
+                {
+                    content: "Cassettes",
+                },
+                {
+                    content: "Stories",
+                },
+                {
+                    content: "Qrates Curated",
+                },
+            ]
+        },
+        {
+            title: "OUR COMPANY",
+            contentData: [
+                {
+                    content: "About Qrates",
+                },
+                {
+                    content: "Careers",
+                },
+                {
+                    content: "Partners",
+                },
+            ]
+        },
+        {
+            title: "HELP",
+            contentData: [
+                {
+                    content: "Support Center",
+                },
+                {
+                    content: "Contact Us",
+                },
+                {
+                    content: "Returns",
+                },
+                {
+                    content: "Shipping",
+                },
+            ]
+        },
+    ]
+    return (
+        <>
+            <div className='footer-section-container'>
+                <div className='footer-section-content'>
+                    {
+                        footerData?.map((data, index) => (
+                            <div className='footer-section-inner-content' key={index}>
+                                <h4>{data?.title}</h4>
+                                {
+                                    data?.contentData?.map((data, index) => (
+                                        <div key={index}>
+                                            <a href='#'>{data?.content}</a>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className='footer-bottom-section-container'>
+                    <div className='footer-bottom-section-img'>
+                        <img src={footerLogo} className='img-fluid' alt='footer-logo' />
+                    </div>
+                    <div className='footer-bottom-section-right-content'>
+                        <button>English</button>
+                        <div className='footer-bottom-section-social-media-section'>
+                            <FontAwesomeIcon icon={faFacebook} className='footer-bottom-social-icon' />
+                            <FontAwesomeIcon icon={faInstagram} className='footer-bottom-social-icon' />
+                            <FontAwesomeIcon icon={faTwitter} className='footer-bottom-social-icon' />
+                        </div>
+                        <div className='footer-bottom-section-terms-section'>
+                            <a href='#'>Terms of Service</a>
+                            <a href='#'>Privacy Policy</a>
+                            <a href='#'>Legal Information</a>
+                            <a href='#'>&copy;TDMS Inc.</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import '../assets/css/header.css';
+import headerLogo from '../assets/images/headerLogo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping, faSearch, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+const Header = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    return (
+        <>
+            <div className='header-section-container'>
+                <div className='header-section-container-left-content'>
+                    <img src={headerLogo} className='img-fluid' alt='header-logo' />
+                    <nav className={`header-nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
+                        <a href='#' onClick={toggleMobileMenu}>discover music</a>
+                        <a href='#' onClick={toggleMobileMenu}>for artists</a>
+                        <a href='#' onClick={toggleMobileMenu}>for fans</a>
+                        <div className={`${isMobileMenuOpen ? 'header-mobile-social-icons' : 'd-none'}`}>
+                            <FontAwesomeIcon icon={faFacebook} className='header-social-icon' />
+                            <FontAwesomeIcon icon={faInstagram} className='header-social-icon' />
+                            <FontAwesomeIcon icon={faTwitter} className='header-social-icon' />
+                        </div>
+                        <div className={`${isMobileMenuOpen ? 'header-section-container-mobile-right-content' : 'd-none'}`}>
+                            <button>make your own</button>
+                            <a href='#'>login</a>
+                        </div>
+                    </nav>
+                    <FontAwesomeIcon icon={faSearch} className='header-search-icon' />
+                    <FontAwesomeIcon icon={faCartShopping} className='header-cart-icon' />
+                    <button className='mobile-menu-toggle' onClick={toggleMobileMenu}>
+                        <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} />
+                    </button>
+                </div>
+                <div className='header-section-container-right-content'>
+                    <a href='#'>login</a>
+                    <button>make your own</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
