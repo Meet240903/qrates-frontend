4d4f05b31f347250282fbe9cf99c9ee53634be16
Meet240903 <meetmangroliya24@gmail.com>
2024-09-26 12:33:51 +0530

    add

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping, faSearch, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { Link } from 'react-router-dom';
+import headerDiscoverMusicDropDownData from '../data/HeaderDiscoverMusicDropDownData';
 
 const Header = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -13,36 +14,52 @@ const Header = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
     };
 
+    const [showDiscoverMusicSubmenu, setShowDiscoverMusicSubmenu] = useState(false);
+
     return (
         <>
             <div className='header-section-container'>
                 <div className='header-section-container-left-content'>
-                    <Link to='/' onClick={()=>setIsMobileMenuOpen(false)}>
+                    <Link to='/' onClick={() => setIsMobileMenuOpen(false)}>
                         <img src={headerLogo} className='img-fluid' alt='header-logo' />
                     </Link>
                     <nav className={`header-nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
-                        <a href='#' onClick={()=>setIsMobileMenuOpen(false)}>discover music</a>
-                        <Link to='/for-artists' onClick={()=>setIsMobileMenuOpen(false)}>for artists</Link>
-                        <Link to='/for-fans' onClick={()=>setIsMobileMenuOpen(false)}>for fans</Link>
+                        <a href='#' className='header-discover-music-link' onClick={() => setShowDiscoverMusicSubmenu(!showDiscoverMusicSubmenu)}>
+                            discover music
+                            <span style={{transform: showDiscoverMusicSubmenu ? 'rotate(-90deg)' : ''}}>&#9662;</span>
+                        </a>
+                        {
+                            showDiscoverMusicSubmenu && <div className='header-discover-music-submenu'>
+                                {
+                                    headerDiscoverMusicDropDownData?.map((data, index) => (
+                                        <div key={index}>
+                                            <li>{data?.listTitle}</li>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        }
+                        <Link to='/for-artists' onClick={() => { setIsMobileMenuOpen(false); setShowDiscoverMusicSubmenu(false) }}>for artists</Link>
+                        <Link to='/for-fans' onClick={() => { setIsMobileMenuOpen(false); setShowDiscoverMusicSubmenu(false) }}>for fans</Link>
                         <div className={`${isMobileMenuOpen ? 'header-mobile-social-icons' : 'd-none'}`}>
                             <FontAwesomeIcon icon={faFacebook} className='header-social-icon' />
                             <FontAwesomeIcon icon={faInstagram} className='header-social-icon' />
                             <FontAwesomeIcon icon={faTwitter} className='header-social-icon' />
                         </div>
                         <div className={`${isMobileMenuOpen ? 'header-section-container-mobile-right-content' : 'd-none'}`}>
-                            <button>make your own</button>
-                            <a href='#'>login</a>
+                            <button onClick={() => setShowDiscoverMusicSubmenu(false)}>make your own</button>
+                            <a href='#' onClick={() => setShowDiscoverMusicSubmenu(false)}>login</a>
                         </div>
                     </nav>
-                    <FontAwesomeIcon icon={faSearch} className='header-search-icon' />
-                    <FontAwesomeIcon icon={faCartShopping} className='header-cart-icon' />
+                    <FontAwesomeIcon icon={faSearch} className='header-search-icon' onClick={() => setShowDiscoverMusicSubmenu(false)} />
+                    <FontAwesomeIcon icon={faCartShopping} className='header-cart-icon' onClick={() => setShowDiscoverMusicSubmenu(false)} />
                     <button className='mobile-menu-toggle' onClick={toggleMobileMenu}>
                         <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} />
                     </button>
                 </div>
                 <div className='header-section-container-right-content'>
-                    <a href='#'>login</a>
-                    <button>make your own</button>
+                    <a href='#' onClick={() => setShowDiscoverMusicSubmenu(false)}>login</a>
+                    <button onClick={() => setShowDiscoverMusicSubmenu(false)}>make your own</button>
                 </div>
             </div>
         </>
---
+++ src/data/HeaderDiscoverMusicDropDownData.js
@@ -0,0 +1,16 @@
+const headerDiscoverMusicDropDownData = [
+    {
+        listTitle: "Records",
+    },
+    {
+        listTitle: "Cassettes",
+    },
+    {
+        listTitle: "Stories",
+    },
+    {
+        listTitle: "Qrates Curated",
+    },
+]
+
+export default headerDiscoverMusicDropDownData;
